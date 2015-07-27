fa197d4d4abff1eb7b2ab72d2063ee245785a243
Chris Fritz <chrisvfritz@gmail.com>
2015-07-27 01:53:33 -0400

    revised events api

--- lib/events_api.rb
+++ lib/events_api.rb
@@ -12,19 +12,22 @@ module Events
     format :json
 
     resource :events do
-      desc "Return a list of upcoming Lansing coding events."
-      get :list do
-        Group.all
-      end
 
-      desc "Returns a list of upcoming events for a specific query."
-      resource :search do
-        params do
-          requires :query, type: String, desc: "Event query."
+      resource :upcoming do
+        desc "Return a list of upcoming Lansing coding events."
+        get :list do
+          Event.upcoming
         end
-        route_param :query do
-          get do
-            Event.next params[:query]
+
+        desc "Returns a list of upcoming events for a specific query."
+        resource :search do
+          params do
+            requires :query, type: String, desc: "Event query."
+          end
+          route_param :query do
+            get do
+              Event.upcoming params[:query]
+            end
           end
         end
       end
@@ -42,7 +45,20 @@ end
 
 class Event
   class << self
-    def next query
+
+    def upcoming query
+      query ? upcoming_search(query) : all_upcoming
+    end
+
+  private
+
+    def all_upcoming
+      Group.all.map do |group|
+        Endpoint.get "https://api.meetup.com/2/events?group_id=#{group['id']}&status=upcoming&page=1&key=#{KEY}"
+      end.flatten
+    end
+
+    def upcoming_search query
       query = case query
       when 'js' then 'javascript'
       when 'rb' then 'ruby'
@@ -58,6 +74,7 @@ class Event
 
       Endpoint.get "https://api.meetup.com/2/events?group_id=#{matching_group['id']}&status=upcoming&page=1&key=#{KEY}"
     end
+
   end
 end
 
