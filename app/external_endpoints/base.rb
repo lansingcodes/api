9128f96af899c5e33b5cc734af9ee96e075bd117
Erik Gillespie <erik.gillespie@gmail.com>
2017-05-04 13:34:15 -0400

    Workaround for Meetup API sometimes returning empty body

--- app/external_endpoints/base.rb
+++ app/external_endpoints/base.rb
@@ -3,10 +3,22 @@ require 'json'
 
 class LansingCodes::ExternalEndpoints::Base
 
+  MAX_RETRIES = 3
+
 private
 
   def get_json
-    JSON.parse( RestClient.get(@url) )
+    json = ''
+    attempts = 0
+
+    # Temporary workaround for https://github.com/meetup/api/issues/220
+    loop do
+      json = RestClient.get(@url)
+      attempts += 1
+      break if attempts >= MAX_RETRIES || (json && json.length >= 2)
+    end
+
+    JSON.parse(json)
   end
 
 end
