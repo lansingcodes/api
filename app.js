fe3396ce327c061291ec4e306cf8447c7cd8ad20
lucidmachine <lucid.machine@gmail.com>
2018-10-03 08:15:07 -0400

    Add app and dummy events routes

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var eventsRouter = require('./routes/events');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/v1/events', eventsRouter);
+
+module.exports = app;
