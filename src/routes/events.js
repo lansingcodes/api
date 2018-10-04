f4980e7a8eb16c6cfa0228fe782919c8d5ef327b
lucidmachine <lucid.machine@gmail.com>
2018-10-03 22:06:12 -0400

    Move app code to src directory

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var eventsController = require('./../controllers/events');
+var eventsController = require('../controllers/events');
 
 router.get('/upcoming/list', eventsController.list);
 router.get('/upcoming/search/:query', eventsController.search);
