5b872b5faf5a75667d733bee6620d19883429e41
lucidmachine <lucid.machine@gmail.com>
2018-10-03 22:07:15 -0400

    Remove static content middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -13,7 +12,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/v1/events', eventsRouter);
 
