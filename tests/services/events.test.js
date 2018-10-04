7d40e519d79bbb9a8350930ee5c7fbea1c04f7fc
lucidmachine <lucid.machine@gmail.com>
2018-10-03 22:35:36 -0400

    Stub unit tests

---
+++ tests/services/events.test.js
@@ -0,0 +1,23 @@
+describe('Events Service', function () {
+
+  describe('Listing', function () {
+    it('returns a list of events when results are found', function () {
+      fail();
+    });
+
+    it('returns an empty list when no results are found', function () {
+      fail();
+    });
+  });
+
+  describe('Search', function () {
+    it('returns a list of events when results are found', function () {
+      fail();
+    });
+
+    it('returns an empty list when no results are found', function () {
+      fail();
+    });
+  });
+
+});
