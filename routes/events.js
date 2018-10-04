c4b621891f11a78e0ee0a6a2ebb853d678a5608a
lucidmachine <lucid.machine@gmail.com>
2018-10-03 21:45:36 -0400

    Create events controller

--- routes/events.js
+++ routes/events.js
@@ -1,26 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var eventsController = require('./../controllers/events');
 
-router.get('/upcoming/list', function (req, res) {
-  res.send([
-    {
-      "name": "Event 1",
-      "description": "This is a very real event. Really."
-    },
-    {
-      "name": "Event 2",
-      "description": "This is another very real event. I wouldn't lie to you."
-    }
-  ]);
-});
-
-router.get('/upcoming/search/:query', function (req, res) {
-  res.send([
-    {
-      "name": "Search Result Event 1",
-      "description": "This description contains the keyword '" + req.params.query + "'!"
-    }
-  ]);
-});
+router.get('/upcoming/list', eventsController.list);
+router.get('/upcoming/search/:query', eventsController.search);
 
 module.exports = router;
