6626c351d1d4663c0028decc816635cfac27bcd2
Eric Arbour <arboureric@gmail.com>
2018-11-01 21:49:45 -0400

    update getTimeDifference event service utility to pass all tests

--- src/services/events/utilities.js
+++ src/services/events/utilities.js
@@ -1,4 +1,4 @@
-const { slugMap } = require("./config");
+const { slugMap } = require('./config');
 
 function mergeGroupEvents(mergedEvents = [], eventsByGroup = {}) {
   return eventsByGroup.results
@@ -42,15 +42,15 @@ function translateEvent(event = {}) {
     relationships: {
       group: event.group
         ? {
-            type: "groups",
-            id: event.group.id || null
-          }
+          type: 'groups',
+          id: event.group.id || null
+        }
         : null,
       venue: event.venue
         ? {
-            type: "venues",
-            id: event.venue.id || null
-          }
+          type: 'venues',
+          id: event.venue.id || null
+        }
         : null
     }
   };
@@ -63,8 +63,8 @@ function makeVenuesObject(events = []) {
       object[venue.id] = {
         attributes: {
           name: venue.name || null,
-          address: `${venue.address_1 || ""}, ${venue.city ||
-            ""}, ${venue.state || ""}`,
+          address: `${venue.address_1 || ''}, ${venue.city ||
+            ''}, ${venue.state || ''}`,
           latitude: venue.lat || null,
           longitude: venue.lon || null,
           directions: venue.how_to_find_us || null
@@ -82,7 +82,7 @@ function makeGroupsObject(events = []) {
       object[group.id] = {
         attributes: {
           name: group.name || null,
-          focus: slugMap[group.urlname] || "General",
+          focus: slugMap[group.urlname] || 'General',
           slug: group.urlname || null,
           members: group.who || null,
           logo: group.group_photo ? group.group_photo.photo_link : null
@@ -94,50 +94,70 @@ function makeGroupsObject(events = []) {
 }
 
 function getRelativeTime(eventUnixTime) {
-  if (!eventUnixTime || typeof eventUnixTime !== "number") return "Unknown";
+  if (!eventUnixTime || typeof eventUnixTime !== 'number') return 'Unknown';
 
   const now = new Date();
 
-  return getTimeDifference(now.getTime(), eventUnixTime);
+  return (
+    getTimeDifference(eventUnixTime, now.getTime()) +
+    (eventUnixTime - now.getTime() < 0 ? ' ago' : '')
+  );
 }
 
 // Lifted from SO and modified for future/past times:
 // https://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time-eg-2-seconds-ago-one-week-ago-etc-best
 // Used in lieu of the Action View - Date Helpers - distance_of_time_in_words
 // from Ruby on Rails
-function getTimeDifference(currentUnixTime, eventUnixTime) {
-  const msPerMinute = 60 * 1000;
-  const msPerHour = msPerMinute * 60;
-  const msPerDay = msPerHour * 24;
-  const msPerMonth = msPerDay * 30;
-  const msPerYear = msPerDay * 365;
+function getTimeDifference(eventUnixTime, currentUnixTime) {
+  const seconds = n => n * 1000;
+  const minutes = n => n * seconds(60);
+  const hours = n => n * minutes(60);
+  const days = n => n * hours(24);
+  const months = n => n * days(30);
+  const years = n => n * days(365);
 
   const msDiff = eventUnixTime - currentUnixTime;
-  const isPassed = msDiff < 0;
   const absDiff = Math.abs(msDiff);
-  const partialGetDiffTimeText = getDiffTimeText.bind(null, absDiff, isPassed);
 
-  if (absDiff < msPerMinute) {
-    return partialGetDiffTimeText(1000, "second");
-  } else if (absDiff < msPerHour) {
-    return partialGetDiffTimeText(msPerMinute, "minute");
-  } else if (absDiff < msPerDay) {
-    return partialGetDiffTimeText(msPerHour, "hour");
-  } else if (absDiff < msPerMonth) {
-    return partialGetDiffTimeText(msPerDay, "day");
-  } else if (absDiff < msPerYear) {
-    return partialGetDiffTimeText(msPerMonth, "month");
+  if (absDiff <= seconds(29)) {
+    return 'less than a minute';
+  } else if (absDiff <= minutes(1) + seconds(29)) {
+    return '1 minute';
+  } else if (absDiff <= minutes(44) + seconds(29)) {
+    return Math.round(absDiff / minutes(1)) + ' minutes';
+  } else if (absDiff <= minutes(89) + seconds(29)) {
+    return 'about 1 hour';
+  } else if (absDiff <= hours(23) + minutes(59) + seconds(29)) {
+    const roundedHours = Math.round(absDiff / hours(1));
+    const numberOfHours = roundedHours === 1 ? 2 : roundedHours;
+    return 'about ' + numberOfHours + ' hours';
+  } else if (absDiff <= hours(41) + minutes(59) + seconds(29)) {
+    return '1 day';
+  } else if (absDiff <= days(29) + hours(23) + minutes(59) + seconds(29)) {
+    const roundedDays = Math.round(absDiff / days(1));
+    const numberOfDays = roundedDays === 30 ? 29 : roundedDays;
+    return numberOfDays + ' days';
+  } else if (absDiff <= days(44) + hours(23) + minutes(59) + seconds(29)) {
+    return 'about 1 month';
+  } else if (absDiff <= days(59) + hours(23) + minutes(59) + seconds(29)) {
+    return 'about 2 months';
+  } else if (absDiff <= years(1) - seconds(1)) {
+    return Math.round(absDiff / months(1)) + ' months';
+  } else if (absDiff <= years(1) + months(3) - seconds(1)) {
+    return 'about 1 year';
+  } else if (absDiff <= years(1) + months(9) - seconds(1)) {
+    return 'over 1 year';
+  } else if (absDiff <= years(2) - seconds(1)) {
+    return 'almost 2 years';
+  } else if (absDiff <= years(2) + months(3) - seconds(1)) {
+    return 'about 2 years';
+  } else if (absDiff <= years(2) + months(9) - seconds(1)) {
+    return 'over 2 years';
+  } else if (absDiff <= years(3) - seconds(1)) {
+    return 'almost 3 years';
   } else {
-    return "about " + partialGetDiffTimeText(msPerYear, "year");
+    return 'over 3 years';
   }
-}
-
-function getDiffTimeText(absDiff, isPassed, divisor, unit) {
-  const value = Math.round(absDiff / divisor);
-  const unitText = value === 1 ? unit : unit + "s";
-  const optionalAgo = isPassed ? " ago" : "";
-
-  return `${value} ${unitText}${optionalAgo}`;
 }
 
 module.exports = {
