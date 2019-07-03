7fbb38a427628cd3a3d557c70c7756721ff77d12
Erik Gillespie <erik.gillespie@gmail.com>
2019-07-03 10:42:22 -0400

    Only initialize firebase-admin once while server is running

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,14 @@
 import admin from 'firebase-admin'
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+let initialized = false
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-})
-
-export default admin
+export default () => {
+  if (!initialized) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
+    })
+    initialized = true
+  }
+  return admin
+}
--- src/firebase/sponsors-set-default.js
+++ src/firebase/sponsors-set-default.js
@@ -3,7 +3,7 @@ import admin from './index'
 const sponsors = require('../../data/sponsors.json')
 
 export default () => {
-  const db = admin.firestore()
+  const db = admin().firestore()
   const sponsorsRef = db.collection('sponsors')
   return Promise.all(
     Object.keys(sponsors).map(key => sponsorsRef.doc(key).set(sponsors[key]))
