5067d2ca4f24864d9a19848a53c137841b131539
Erik Gillespie <erik.gillespie@gmail.com>
2019-07-02 21:01:17 -0400

    Out with the old

--- deploy/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $HOME
-
-sudo $(aws ecr get-login --region us-east-1)
-sudo docker-compose pull lcapi
-sudo docker-compose stop lcapi
-sudo docker-compose rm -f lcapi
-sudo docker-compose up -d
--- spec/support/coverage.rb
+++
@@ -1,7 +0,0 @@
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-else
-  require 'simplecov'
-  SimpleCov.start
-end
