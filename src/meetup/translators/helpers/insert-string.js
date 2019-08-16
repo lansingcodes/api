542fc66c246f8165965f6f7345399102680bb9fb
Erik Gillespie <erik.gillespie@gmail.com>
2019-08-15 22:24:51 -0400

    Remove unused utility

--- src/meetup/translators/helpers/insert-string.js
+++
@@ -1,7 +0,0 @@
-// Inserts a value into a string at the specified index
-// Example:
-//   insertString('hello world', 5, ',')
-//   'hello, world'
-
-module.exports = (str, index, value) =>
-  str.substr(0, index) + value + str.substr(index)
